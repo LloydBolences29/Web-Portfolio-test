4128f0d47328154256fb4f8c2e8d8412e6f950ca
LloydBolences29 <lmbolences@gmail.com>
2025-10-08 18:37:09 +0800

    added some file for Ci/Cd workflow

---
+++ portfolio/scripts/deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+sudo -i bash <<EOF
+echo "***********************************"
+echo "Navigate to the docker folder"
+echo "***********************************"
+cd /home/ubuntu/web-app/Web-Portfolio-test/portfolio
+
+echo "***********************************"
+echo "Login to AWS ECR"
+echo "***********************************"
+aws ecr get-login-password --region <region> | docker login --username AWS --password-stdin <account_id>.dkr.ecr.<region>.amazonaws.com
+
+echo "***********************************"
+echo "Stop the application"
+echo "***********************************"
+docker compose down
+
+echo "***********************************"
+echo "Remove all dangling and unused docker images"
+echo "***********************************"
+docker image prune -af
+
+echo "***********************************"
+echo "Pull latest images from repository"
+echo "***********************************"
+docker compose pull
+
+echo "***********************************"
+echo "Start the application"
+echo "***********************************"
+docker compose up -d
+
+echo "***********************************"
+echo "Application deployment completed successfully!"
+echo "***********************************"
+EOF
